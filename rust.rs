73da73a14b095a2d30f81e9c9c928842d46f0bc0
Ian Seyler <ian@ians-mac-mini.local>
2025-06-18 14:20:14 -0400

    Created `b_output` wrapper

--- rust.rs
+++ rust.rs
@@ -1,4 +1,4 @@
-// Rust on BareMetal
+// Rust on BareMetal - tested on rust 1.87.0
 // rustc -O --crate-type lib -o rust.o --emit obj rust.rs
 // ld -T app.ld -o rust.app rust.o
 
@@ -15,18 +15,23 @@ pub extern "C" fn _start()
 {
 	let s = "\nHello world from Rust!";
 
-	// Output the string via the kernel API
-	unsafe
-	{
-		asm!(
-			"mov rsi, {0}", // string address
-			"mov rcx, {1}", // string length
-			"call [0x00100018]", // b_output
-			in(reg) s.as_ptr(),
-			in(reg) s.len()
-		);
-	}
+	b_output(s.as_ptr(), s.len());
 
+}
+
+// BareMetal b_output wrapper
+fn b_output(string_pointer: *const u8, string_length: usize)
+{
+        unsafe
+        {
+                asm!(
+                        "mov rsi, {0}", // string address
+                        "mov rcx, {1}", // string length
+                        "call [0x00100018]", // b_output
+                        in(reg) {string_pointer},
+                        in(reg) {string_length}
+                );
+        }
 }
 
 // This function is called during panic
